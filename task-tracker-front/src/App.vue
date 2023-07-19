b4eefbb34dce17d405d9efaf8c7f79604555e2f1
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-07-19 18:46:35 +0300

    init vue app

---
+++ task-tracker-front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello World",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
