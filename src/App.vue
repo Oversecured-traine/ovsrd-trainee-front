5efcfc19a726c15d89653642507f60fda7a45a72
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-07-24 21:42:37 +0300

    added drawer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Header from './components/Header.vue';
-
+import Drawer from './components/Drawer.vue';
 
 </script>
 
 <template>
     <v-app>
         <Header/>
+        <Drawer/>
     </v-app>
 </template>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <v-navigation-drawer
+        expand-on-hover
+        rail
+        image="https://picsum.photos/id/120/4928/3264"
+        theme="dark"
+    >
+        <v-list nav>
+            <v-list-item
+                prepend-icon="mdi-home-circle-outline"
+                title="Home"
+                value="home"
+            ></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
