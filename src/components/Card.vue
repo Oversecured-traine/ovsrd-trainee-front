f252276c2f2babd4b3a18b8850ee5a00cbe74510
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-07-31 20:59:11 +0300

    updated card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,80 +1,118 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <v-card class="card">
-        <v-card-title @click="openModal">{{ cardCopy.title }}</v-card-title>
-        <v-btn
-            icon="mdi-trash-can-outline"
-            variant="text"
-            @click="deleteCard"
-        ></v-btn>
-        <v-dialog v-model="showModal" max-width="500">
+    <li>
+        <div class="card-item" @click="openModal = true">
+            <h4>
+                <slot> </slot>
+            </h4>
+            <button @click="deleteCard" class="delete-card-btn">
+                <v-icon icon="mdi-archive" size="x-small"></v-icon>
+            </button>
+        </div>
+        <!--Modal Window -->
+        <v-dialog v-model="openModal" max-width="500">
             <v-card>
                 <v-card-title>
                     <span class="headline">Edit Card</span>
                 </v-card-title>
                 <v-card-text>
-                    <v-text-field
-                        v-model="cardCopy.title"
-                        label="Title"
-                    ></v-text-field>
-                    <v-textarea
-                        v-model="cardCopy.description"
-                        label="Description"
-                    ></v-textarea>
+                    <v-text-field label="Title" v-model="newTitle" @input="editCardTitle($event)">
+                    </v-text-field>
+                    <v-textarea label="Description" v-model="newDescription" @input="editCardDescription($event)">
+                    </v-textarea>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" text @click="closeModal">Close</v-btn>
+                    <v-btn color="primary" text @click="openModal = false">Close</v-btn>
                     <v-btn color="primary" text @click="saveCard">Save</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
-    </v-card>
+    </li>
 </template>
 
 <script>
+
 export default {
-    props: {
-        card: Object,
-        cardIndex: Number,
-    },
     data() {
         return {
-            cardCopy: {},
-            showModal: false,
+            openModal: false,
+            newTitle: this.card.title,
+            newDescription: this.card.description,
         };
     },
-    watch: {
-        card: {
-            handler(newVal) {
-                this.cardCopy = JSON.parse(JSON.stringify(newVal)); 
-            },
-            immediate: true,
-        },
+    props: {
+        card: Object,
+        column: Object,
     },
-
     methods: {
-        openModal() {
-            this.showModal = true;
-        },
         deleteCard() {
-            this.$emit('delete-card', this.cardCopy);
+            this.$emit('delete-card', this.card.key, this.column.key);
         },
+
         saveCard() {
-            this.$emit('edit-card', this.cardCopy);
-            this.showModal = false;
+            this.$emit(
+                'save-card',
+                this.card.key,
+                this.column.key,
+                this.newTitle,
+                this.newDescription,
+            );
+            this.openModal = false;
         },
-        closeModal() {
-            this.showModal = false;
+
+        editCardTitle() {
+            this.newTitle = event.target.value;
+        },
+        editCardDescription() {
+            this.newDescription = event.target.value;
         },
     },
 };
 </script>
 
 <style scoped>
-.card {
-    margin-bottom: 10px;
-    width: 100%;
 
+h4 {
+    padding: 0.3rem;
+    color: #4a5568;
+}
+
+.card-item {
+    font-size: 0.875rem;
+}
+
+li {
+    position: relative;
+    background-color: #fff;
+    padding: 0.75rem;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+    border-radius: 0.375rem;
+    border-bottom-width: 1px;
+    border-color: #e2e8f0;
+}
+
+li:hover {
+    background-color: #f7fafc;
+}
+
+.delete-card-btn {
+    display: none;
+    position: absolute;
+    top: 0.25rem;
+    right: 0.25rem;
+    width: 2rem;
+    height: 2rem;
+    background-color: #f7fafc;
+    place-content: center;
+    border-radius: 0.375rem;
+    color: #718096;
+}
+
+.delete-card-btn:hover {
+    background-color: #edf2f7;
+}
+li:hover .delete-card-btn {
+    display: grid;
 }
 </style>
