c795f84f9dd3780ca05887d05503aaac31185c69
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-07-31 21:01:21 +0300

    updated column component

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,89 +1,142 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <v-col :key="columnIndex" class="column">
-        <h3 contenteditable @input="editColumnTitle($event)">
-            {{ columnCopy.title }}
-        </h3>
-        <v-row>
-            <v-col
-                class="card-column">
-                <Card
-                    v-for="(card, cardIndex) in columnCopy.cards"
-                    :key="cardIndex"
-                    :card="card"
-                    :card-index="cardIndex"
-                    @edit-card="editCard(cardIndex, card)"
-                    @delete-card="deleteCard(cardIndex)"
-                />
-            </v-col>
-            <v-col class="text-right">
-                <v-btn color="primary" depressed @click="addCard">Add Card</v-btn>
-                <v-btn
-                    icon="mdi-delete-outline"
-                    variant="text"
-                    @click="deleteColumn"
-                ></v-btn>
-            </v-col>
-        </v-row>
-    </v-col>
+    <div class="column">
+        <div class="column-title">
+            <input @input="editColumnTitle($event)" placeholder="Enter list name"> 
+            <!--Dropdown Menu-->           
+            <v-menu offset-y>
+                <template v-slot:activator="{ props }">
+                    <button class="column-actions-btn" v-bind="props">
+                        <v-icon icon="mdi-dots-horizontal" size="small"></v-icon>
+                    </button>
+                </template>
+                <v-list>
+                    <v-list-item @click="deleteColumn">
+                        <v-icon icon="mdi-delete" size="small"></v-icon>
+                        Delete list                       
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </div>
+        <div class="card-list">
+            <div class="scroll-section">
+                <ul>
+                    <slot></slot>
+                </ul>
+            </div>
+            <div class="add-btn-container">
+                <button class="add-btn" @click="addCard">
+                    <v-icon icon="mdi-plus"></v-icon>
+                    <span style="margin-left: 0.25rem">Add Card</span>
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Card from './Card.vue';
 
 export default {
-    components: {
-        Card,
-    },
     props: {
         column: Object,
-        columnIndex: Number,
-    },
-    data() {
-        return {
-            columnCopy: {},
-        };
-    },
-    watch: {
-        column: {
-            handler(newVal) {
-                this.columnCopy = JSON.parse(JSON.stringify(newVal));
-            },
-            immediate: true,
-        },
     },
     methods: {
-        editColumnTitle(event) {
-            const title = event.target.innerText;
-            this.$emit('update-column-title', this.columnIndex, title);
-        },
         addCard() {
-            this.columnCopy.cards.push({
-                cardIndex: this.columnCopy.cards.length,
-                title: 'New Card',
-                description: '',
-            });
+            this.$emit('add-card', this.column.key);
         },
-        editCard(cardIndex, updatedCard) {
-            this.columnCopy.cards.splice(cardIndex, 1, updatedCard);
+
+        editColumnTitle(event) {
+            this.$emit(
+                'update-column-title',
+                this.column.key,
+                event.target.value,
+            );
         },
+
         deleteColumn() {
-            this.$emit('delete-column');
+            this.$emit('delete-column', this.column.key);
         },
-        deleteCard(cardIndex) {
-            this.columnCopy.cards.splice(cardIndex, 1);
-        },
+        
     },
 };
 </script>
 
 <style scoped>
-.column {
-    width: 320px;
-    padding: 10px;
-    background-color: #f5f5f5;
-    border-radius: 5px;
-    margin-right: 10px;
+
+ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
 }
 
+.column {
+    width: 18rem;
+    background-color: #cbd5e0;
+    display: flex;
+    flex-direction: column;
+    border-radius: 0.375rem;
+    max-height: 100%;
+}
+
+.column-title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.5rem 0.75rem;
+}
+
+.column-actions-btn {
+    background-color: transparent;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 0.375rem;
+    place-content: center;
+}
+
+.column-actions-btn:hover {
+    background-color: #c8ccd3;
+}
+
+.card-list {
+    padding: 0 0.75rem 0.75rem;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+}
+
+.scroll-section {
+    overflow-y: auto;
+    flex: 1;
+    padding-left: 0.25rem;
+    padding-right: 0.75rem;
+}
+
+.add-btn {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    font-size: 0.875rem;
+    font-weight: 500;
+    color: #718096;
+    border-radius: 0.375rem;
+    transition: background-color 0.3s, color 0.3s;
+    width: 100%;
+}
+
+.add-btn:hover {
+    background-color: #edf2f7;
+    color: #000;
+}
+
+.add-btn-container {
+    margin-top: 0.75rem;
+}
+
+input {
+    font-size: 1rem;
+    font-weight: 600;
+    color: #4a5568;
+    padding: 0.3rem;
+}
 </style>
