6bf795cfdd98304b804297b1ba4b1730933f6daa
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-07-31 20:54:48 +0300

    deleted header and drawer components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
-import Header from './components/Header.vue';
-import Drawer from './components/Drawer.vue';
 import Board from './components/Board.vue';
 
 </script>
 
 <template>
     <v-app>
-        <Header/>
-        <Drawer/>
+
         <v-main>
             <Board/>
         </v-main>
