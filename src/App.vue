4ec9b970d2bc627800817c4cfec6f7487a7492d0
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-08-18 01:49:49 +0300

    added a loading spinner

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <v-app>
 
         <v-main>
+            <LoadingSpinner v-if="isLoading"/>
             <Board/>
         </v-main>
     </v-app>
@@ -9,11 +10,16 @@
 
 <script>
 import Board from './components/Board.vue';
-
+import LoadingSpinner from './components/LoadingSpinner.vue';
+import { mapGetters } from 'vuex';
 export default {
     components: {
         Board,
+        LoadingSpinner,
     },
+    computed: {
+        ...mapGetters(['isLoading']),
+    }, 
 
 };
 
