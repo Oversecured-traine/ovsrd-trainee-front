b16b55a119d334387560aceed9eef98bcac29241
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-09-03 01:28:09 +0300

    changed modal buttons style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -40,8 +40,8 @@
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" text @click="openModal = false">Close</v-btn>
-                    <v-btn color="primary" text @click="saveCard">Save</v-btn>
+                    <v-btn color="primary" variant="tonal" @click="openModal = false">Close</v-btn>
+                    <v-btn color="primary" variant="tonal" @click="saveCard">Save</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
