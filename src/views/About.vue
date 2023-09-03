1a6355ec812bb479cb72e40795fac787fdbe9147
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-09-04 01:09:45 +0300

    created about page

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+
+    <div>
+        <h1>About Page</h1>
+        <p>This is the about page content.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'About',
+};
+</script>
